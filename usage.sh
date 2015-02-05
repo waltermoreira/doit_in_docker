ea26616b82292f347c48f8dca3a315569ecb29a7
Walter Moreira <walter@waltermoreira.net>
2015-02-05 14:50:07 -0600

    Fully qualify name of image

--- usage.sh
+++ usage.sh
@@ -2,14 +2,14 @@
 
 echo "Run as:"
 echo ""
-echo '    docker run -it -v $(pwd):/target doit_in_docker doit ...'
+echo '    docker run -it -v $(pwd):/target waltermoreira/doit_in_docker doit ...'
 echo ""
 echo "If you need docker inside, add '-v /var/run/docker.sock:/docker.sock'"
 echo ""
 echo "For easy invocation, if you are brave, let us create an alias for you."
 echo "Execute"
 echo ""
-echo '    eval $(docker run doit_in_docker /alias)'
+echo '    eval $(docker run waltermoreira/doit_in_docker /alias)'
 echo ""
 echo "and then use the command 'doit_in_docker'. For example:"
 echo ""
