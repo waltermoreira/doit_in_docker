a744d61110ec1adbe4236b1a90e85ac0e8e4997b
Walter Moreira <walter@waltermoreira.net>
2015-02-15 14:26:12 -0600

    Fix alias script

--- alias.sh
+++ alias.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "alias doit_in_docker='docker run -it -v \$(pwd):/target -v /var/run/docker.sock:/docker.sock waltermoreira/doit_in_docker'"
-echo "alias doit='docker run -it -v \$(pwd):/target -v /var/run/docker.sock:/docker.sock waltermoreira/doit_in_docker doit'"
+echo "alias doit_in_docker='docker run -it -v \$(pwd):/target -v /var/run/docker.sock:/docker.sock waltermoreira/doit_in_docker';"
+echo "alias doit='docker run -it -v \$(pwd):/target -v /var/run/docker.sock:/docker.sock waltermoreira/doit_in_docker doit';"
